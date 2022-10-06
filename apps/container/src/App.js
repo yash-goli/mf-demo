75f297160f6492ffb930289e1c6b9137bb646858
Yaswanth Goli <dev.yashu58@hotmail.com>
2022-10-06 18:21:02 +0200

    Added header

--- apps/container/src/App.js
+++ apps/container/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+  );
 };
